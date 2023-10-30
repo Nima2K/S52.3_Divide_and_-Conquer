a42adc3fc8f4f773641dc885b483b47f0d101599
Nima Najaflou <59122448+Nima2K@users.noreply.github.com>
2023-10-30 19:25:50 +0000

    NA

--- countZeroes.js
+++ countZeroes.js
@@ -15,8 +15,8 @@ function countZeroes(arr) {
   }
   
   // Examples
-  console.log(countZeroes([1,1,1,1,0,0])); // Output: 2
-  console.log(countZeroes([1,0,0,0,0]));   // Output: 4
-  console.log(countZeroes([0,0,0]));       // Output: 3
-  console.log(countZeroes([1,1,1,1]));     // Output: 0
+  console.log(countZeroes([1,1,1,1,0,0])); 
+  console.log(countZeroes([1,0,0,0,0]));   
+  console.log(countZeroes([0,0,0]));    
+  console.log(countZeroes([1,1,1,1]));
   
